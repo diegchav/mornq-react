c334354d365e2089f10f0ae9c52d4a30955b90f8
Diego Chavez <diegchav.me@gmail.com>
2020-04-14 06:25:19 -0500

    Add global styles using styled components

--- src/App.js
+++ src/App.js
@@ -1,8 +1,12 @@
 import React from 'react';
 
+import GlobalStyle from './global.styles';
+
 function App() {
   return (
-    <div></div>
+    <React.Fragment>
+      <GlobalStyle />
+    </React.Fragment>
   );
 }
 
---
+++ src/global.styles.js
@@ -0,0 +1,13 @@
+import { createGlobalStyle } from 'styled-components';
+
+const GlobalStyle = createGlobalStyle`
+    body {
+        margin: 0;
+        padding: 0;
+        width: 100vw;
+        height: 100vh;
+        font-family: Helvetica, Arial, sans-serif;
+    }
+`;
+
+export default GlobalStyle;
